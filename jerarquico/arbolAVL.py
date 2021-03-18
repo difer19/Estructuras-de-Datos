72ee203a376ce382ad90874445b85ae85635929e
difer19 <63697909+difer19@users.noreply.github.com>
2021-03-18 10:49:51 -0500

    ArbolAVL completed

--- jerarquico/arbolAVL.py
+++ jerarquico/arbolAVL.py
@@ -2,9 +2,5 @@ from jerarquico.nodoArbolBinario import NodoArbolBinario
 
 
 class ArbolAVL:
-    """
-    Implementacion de la estructura de datos llamada
-    ArbolAVL
-    """
     def __init__(self):
         pass
